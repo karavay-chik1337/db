5f1df96bcc5cc421736e740c59d8fa118de1cba0
Karavaev2024 <dima_karavaev04@mail.ru>
2024-05-13 12:35:23 +0300

    #3

--- 33-52.sql
+++ 33-52.sql
@@ -11,10 +11,21 @@
 -- limit 1)) and model.mark_id = mark.mark_id;
 
 --40
-select mark.mark_name, model.model_name
-from mark,
-     model
-where model.model_id = (select model_id
-                        from car
-                        group by model_id)
-  and model.mark_id = mark.mark_id;
+WITH RankedCars AS (
+    SELECT
+        m.mark_name,
+        c.car_yearOfProduction,
+        ROW_NUMBER() OVER (PARTITION BY m.mark_name ORDER BY c.car_yearOfProduction) AS oldest_rank,
+        ROW_NUMBER() OVER (PARTITION BY m.mark_name ORDER BY c.car_yearOfProduction DESC) AS newest_rank
+    FROM car c
+             JOIN model mo ON c.model_id = mo.model_id
+             JOIN mark m ON mo.mark_id = m.mark_id
+)
+SELECT
+    mark_name,
+    MIN(car_yearOfProduction) AS oldest_car_year,
+    MAX(car_yearOfProduction) AS newest_car_year
+FROM RankedCars
+WHERE oldest_rank = 1 OR newest_rank = 1
+GROUP BY mark_name
+ORDER BY mark_name;
--- insert.sql
+++ insert.sql
@@ -1,51 +1,51 @@
--- insert into city(city_id, city_name)
--- VALUES (1, 'Voronezh');
---
--- insert into street(street_id, city_id, street_name)
--- VALUES (1, 1, 'Voroshilova'),
---        (2, 1, 'Gramshi');
+insert into city(city_id, city_name)
+VALUES (1, 'Voronezh');
 
--- insert into house(house_id, street_id, house_number, office_number)
--- VALUES (1, 1, '49', null),
---        (2, 1, '50', null),
---        (3, 1, '30', null),
---        (4, 2, '40', 2),
---        (5, 2, '40', 3),
---        (6, 2, '40', 4);
+insert into street(street_id, city_id, street_name)
+VALUES (1, 1, 'Voroshilova'),
+       (2, 1, 'Gramshi');
 
--- insert into mark(mark_id, mark_name)
--- VALUES (1, 'Mazda'),
---        (2, 'Renault'),
---        (3, 'Kia'),
---        (4, 'Nissan'),
---        (5, 'Toyota');
---
--- insert into model(model_id, mark_id, model_name)
--- VALUES (1, 1, 'CX-5'),
---        (2, 1, 'RX-7'),
---        (3, 2, 'Logan'),
---        (4, 2, 'Megan'),
---        (5, 3, 'Rio'),
---        (6, 3, 'Ceed'),
---        (7, 4, 'Qashqai'),
---        (8, 4, 'X-Trail'),
---        (9, 5, 'Camry'),
---        (10, 5, 'Corolla');
+insert into house(house_id, street_id, house_number, office_number)
+VALUES (1, 1, '49', null),
+       (2, 1, '50', null),
+       (3, 1, '30', null),
+       (4, 2, '40', 2),
+       (5, 2, '40', 3),
+       (6, 2, '40', 4);
 
--- insert into taxi_company_owner(taxi_company_owner_id, taxi_company_owner_name, taxi_company_owner_surname, taxi_company_owner_patronymic, taxi_company_owner_phone, taxi_company_owner_birthday)
--- VALUES (1, 'Alex', 'Korolev', 'Petrovich', 89006001234, '21/04/1970'),
---        (2, 'Dmitriy', 'Karavaev', 'Alekseevich', 89006002345, '07/01/2004'),
---        (3, 'David', 'Davidov', 'Davidovich', 89006003456, '12/07/1991');
+insert into mark(mark_id, mark_name)
+VALUES (1, 'Mazda'),
+       (2, 'Renault'),
+       (3, 'Kia'),
+       (4, 'Nissan'),
+       (5, 'Toyota');
 
--- insert into taxi_company(taxi_company_id, taxi_company_owner_id, house_id, taxi_company_name, taxi_company_phone)
--- values (1, 1, 4, 'Honey and honey', 545450),
---        (2, 2, 5, 'Speed', 515115),
---        (3, 3, 6, 'Forsazh', 909091);
+insert into model(model_id, mark_id, model_name)
+VALUES (1, 1, 'CX-5'),
+       (2, 1, 'RX-7'),
+       (3, 2, 'Logan'),
+       (4, 2, 'Megan'),
+       (5, 3, 'Rio'),
+       (6, 3, 'Ceed'),
+       (7, 4, 'Qashqai'),
+       (8, 4, 'X-Trail'),
+       (9, 5, 'Camry'),
+       (10, 5, 'Corolla');
 
--- insert into client(client_id, client_phone, client_name, client_patronymic)
--- values (1, 89006001020, 'Ivan', null),
---        (2, 89006002030, 'Maks', null),
---        (3, 89006003040, 'Alex', null);
+insert into taxi_company_owner(taxi_company_owner_id, taxi_company_owner_name, taxi_company_owner_surname, taxi_company_owner_patronymic, taxi_company_owner_phone, taxi_company_owner_birthday)
+VALUES (1, 'Alex', 'Korolev', 'Petrovich', 89006001234, '21/04/1970'),
+       (2, 'Dmitriy', 'Karavaev', 'Alekseevich', 89006002345, '07/01/2004'),
+       (3, 'David', 'Davidov', 'Davidovich', 89006003456, '12/07/1991');
+
+insert into taxi_company(taxi_company_id, taxi_company_owner_id, house_id, taxi_company_name, taxi_company_phone)
+values (1, 1, 4, 'Honey and honey', 545450),
+       (2, 2, 5, 'Speed', 515115),
+       (3, 3, 6, 'Forsazh', 909091);
+
+insert into client(client_id, client_phone, client_name, client_patronymic)
+values (1, 89006001020, 'Ivan', null),
+       (2, 89006002030, 'Maks', null),
+       (3, 89006003040, 'Alex', null);
 
 insert into car(car_id, taxi_company_id, model_id, car_yearofproduction, car_number, car_insurance)
 values (1, 1, 3, '01/07/2007', 'o700ct36', '111111111'),
